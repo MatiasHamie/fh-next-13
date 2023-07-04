f2c1b2f2d28f172e72d42fe41a39b86896119e83
Matias Hamie <matias@bytandym.com>
2023-07-04 18:04:52 -0300

    first commit

---
+++ 01-first-steps/app/(general)/contact/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact Page",
+  description: "Esta es la página de contacto de la empresa",
+  keywords: ["Contact Page", "Matias", "Otra Cosa"],
+};
+
+export default function ContactPage() {
+  return (
+    <>
+      <span className="text-7xl">Contact Page</span>
+    </>
+  );
+}
